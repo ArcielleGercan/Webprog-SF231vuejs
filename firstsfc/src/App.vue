4246a8ae83d7dad16aadd4965ed1647f9907e994
ArcielleGercan <acgercan@student.apc.edu.ph>
2025-01-23 14:44:37 +0800

    food item vue

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -4,4 +4,19 @@
 <h1>Hello WEBPROG SF231 World!</h1>
 </template>
 
-<style></style>
+<template>
+    <h1>Food</h1>
+    <food-item/>
+    <food-item/>
+    <food-item/>
+  </template>
+
+<style>
+  #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+</style>
