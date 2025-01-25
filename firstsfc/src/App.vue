312b7c0262834a391dcd74fa5178c61b638f0ceb
ArcielleGercan <acgercan@student.apc.edu.ph>
2025-01-25 23:10:50 +0800

    food item

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -2,10 +2,12 @@
 
 <template>
    <h1>Food</h1>
-  <food-item/> <br>
-  <food-item2/> <br>
-  <food-item/>  <br>
-  <food-item2/> <br>
+  <food-item/>
+  <food-item/>
+  <food-item/>
+  <food-item/>
+  <food-item/>
+  <food-item/>
 </template>
 
 <style></style>
