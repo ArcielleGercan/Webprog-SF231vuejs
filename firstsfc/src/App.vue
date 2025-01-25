10d2865b722e8a27d67f7904debabae7c953340c
ArcielleGercan <acgercan@student.apc.edu.ph>
2025-01-25 22:55:41 +0800

    Food Item

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,6 +1,11 @@
 <script></script>
 
 <template>
-<h1>Hello WEBPROG SF231 World!</h1>
+   <h1>Food</h1>
+  <food-item/> <br>
+  <food-item2/> <br>
+  <food-item/>  <br>
+  <food-item2/> <br>
 </template>
 
+<style></style>
